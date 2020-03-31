2a741cdef178932cb23d9145b1ce7faf56e2fa7b
xiaoguoguo <1662106538@qq.com>
2020-03-31 20:55:24 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import fastclick from 'fastclick'
 import App from './App.vue'
 import routes from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
 Vue.use(VueRouter)
+fastclick.attach(document.body);
 Vue.config.productionTip = false
 const router = new VueRouter({
   routes // (缩写) 相当于 routes: routes
